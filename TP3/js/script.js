93af3ead138812116f8694401d55fed665783d20
ValentinSvedas <valentinsvedas@gmail.com>
2021-10-23 14:54:54 -0300

    Añadido meteorito y cambiado fondo

--- TP3/js/script.js
+++ TP3/js/script.js
@@ -1,5 +1,7 @@
 
 let bird = document.querySelector('.bird');
+let meteoro = document.getElementById("meteoro");
+let volando = false;
 
 function initGame(){
    
@@ -11,14 +13,19 @@ function startGame(){
 }
 
 async function jump(){
-    bird.classList.add("pjvolar")
-    setTimeout(()=>{
-         bird.classList.remove("pjvolar")
-         bird.classList.add("pjcaer")
-    },1000)
-    setTimeout(()=>{
-        bird.classList.remove("pjcaer")
-    },1600)
+    if (volando == false) {
+        volando = true;
+        bird.classList.add("pjvolar")
+        setTimeout(()=>{
+             bird.classList.remove("pjvolar")
+             bird.classList.add("pjcaer")
+        },1000)
+        setTimeout(()=>{
+            bird.classList.remove("pjcaer")
+            volando=false;
+        },1600)
+    }
+   
 }
 
 
